ac6da74960dae24819565c75e29215766051a27c
13xforever <ilya.veselov@gmail.com>
2009-08-23 16:17:14 +0600

    Made some progress in ADEPT key retrieval.

---
+++ Drm.Tests/AdeptTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using Drm.Adept;
+using NUnit.Framework;
+
+namespace Drm.Tests
+{
+	[TestFixture]
+	public class AdeptTests
+	{
+		[Test]
+		public void MakeEntropyTest()
+		{
+			const long systemDriveSerial = 942549858L;
+			const string cpuVendor = "GenuineIntel";
+			var signature = new byte[] {0x00, 0x06, 0xf7};
+			const string username = "13xforever";
+			byte[] entropy = KeyRetriever.MakeEntropy(systemDriveSerial, cpuVendor, signature, username);
+			const string expected = @"8.+bGenuineIntel\x00\x06\xf713xforever\x00\x00\x00";
+			Assert.AreEqual(expected, Encoding.ASCII.GetString(entropy));
+		}
+	}
+}
--- Drm/Adept/KeyRetriever.cs
+++ Drm/Adept/KeyRetriever.cs
@@ -1,22 +1,25 @@
 ﻿using System;
+using System.Linq;
 using System.Management;
+using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.Win32;
 
 namespace Drm.Adept
 {
-	public class KeyRetriever
+	public static class KeyRetriever
 	{
-		public void Retrieve()
+		public static void Retrieve()
 		{
-			string systemDriveSerial = GetSystemDriveSerialNumber();
+			ulong systemDriveSerial = GetSystemDriveSerialNumber();
 			string cpuVendor = GetCpuVendor();
-			string signature = MakeSignature();
+			byte[] signature = MakeSignature();
 			string username = Environment.UserName;
 			byte[] entropy = MakeEntropy(systemDriveSerial, cpuVendor, signature, username);
 			RegistryKey deviceData = Registry.CurrentUser.OpenSubKey(DeviceKey);
 			if (deviceData == null) throw new InvalidOperationException("Adobe Digital Editions isn't activated");
-			var device = (byte[])deviceData.GetValue("key");
-			var decryptedKey = CryptUnprotectData(device, entropy);
+			var device = (byte[])deviceData.GetValue("key"); //\x01\x00\x00\x00\xd0\x8c\x9...01UD,\x90\xbf*1\xbd\xd1{\xe4
+			byte[] decryptedKey = CryptUnprotectData(device, entropy);
 		}
 
 		private static byte[] CryptUnprotectData(byte[] device, byte[] entropy)
@@ -24,31 +27,62 @@ namespace Drm.Adept
 			throw new NotImplementedException();
 		}
 
-		private static byte[] MakeEntropy(string serial, string vendor, string signature, string user)
+#if DEBUG
+		public
+#else
+		private
+#endif
+			static byte[] MakeEntropy(ulong serial, string vendor, byte[] signature, string user)
 		{
-			//entropy = pack('>I12s3s13s', serial, vendor, signature, user)
-			throw new NotImplementedException();
+			//4 bytes
+			var result = new byte[32];
+			BitConverter.GetBytes((uint)serial).Reverse().ToArray().CopyTo(result, 0);
+			//12 bytes
+			Encoding.ASCII.GetBytes(vendor).CopyTo(result, 4);
+			//3 bytes
+			signature.CopyTo(result, 16);
+			//13 bytes
+			Encoding.Unicode.GetBytes(user).Take(26).Where((value, idx) => idx % 2 == 0).ToArray().CopyTo(result, 19);
+			return result;
 		}
 
-		private static string MakeSignature()
+		private static byte[] MakeSignature()
 		{
-			//CPUID1_INSNS = create_string_buffer("\x53\x31\xc0\x40\x0f\xa2\x5b\xc3")
-			//signature = pack('>I', cpuid1())[1:]
-			throw new NotImplementedException();
+			string sig;
+			using (var cpu = new ManagementObject(@"Win32_Processor.DeviceID=""CPU0"""))
+			{
+				cpu.Get();
+				sig = cpu["ProcessorId"].ToString();
+			}
+			var result = new byte[3];
+			for (byte i = 0; i < 3; i++) result[i] = Convert.ToByte(sig.Substring(10 + i * 2, 2), 16);
+			return result;
 		}
 
 		private static string GetCpuVendor()
 		{
-			throw new NotImplementedException();
+			using (var cpu = new ManagementObject(@"Win32_Processor.DeviceID=""CPU0"""))
+			{
+				cpu.Get();
+				return cpu["Manufacturer"].ToString();
+			}
 		}
 
-		private static string GetSystemDriveSerialNumber()
+		private static ulong GetSystemDriveSerialNumber()
 		{
 			char systemDriveLetter = Environment.SystemDirectory[0];
-			var dsk = new ManagementObject(@"win32_logicaldisk.deviceid=""" + systemDriveLetter + @":""");
-			dsk.Get();
-			return dsk["VolumeSerialNumber"].ToString();
+			string numberAsHexString;
+			using (var dsk = new ManagementObject(@"Win32_LogicalDisk.DeviceID=""" + systemDriveLetter + @":"""))
+			{
+				dsk.Get();
+				numberAsHexString = dsk["VolumeSerialNumber"].ToString();
+			}
+			if (!HexString.IsMatch(numberAsHexString))
+				throw new ArgumentException(string.Format("Serial number for volume {0} isn't a valid hexadecimal string ({1})", systemDriveLetter, numberAsHexString));
+			return Convert.ToUInt64(numberAsHexString, 16);
 		}
+
+		private static readonly Regex HexString = new Regex(@"\A\b[0-9a-fA-F]+\b\Z");
 
 		private const string DeviceKey = "Software\\Adobe\\Adept\\Device";
 		private const string PrivateLicenseKey = "Software\\Adobe\\Adept\\Activation\\{0}\\{1}";
