8d1cb05d720180e2cfde18522794673116f61ee7
13xforever <ilya.veselov@gmail.com>
2015-12-19 21:12:24 +0500

    + Kobo Desktop 3.19.3665 encryption scheme

--- Drm/Format/Epub/KoboMasterKeys.cs
+++ Drm/Format/Epub/KoboMasterKeys.cs
@@ -10,67 +10,38 @@ namespace Drm.Format.Epub
 {
 	public static class KoboMasterKeys
 	{
-		private static readonly string[] Salts = { "XzUhGYdFp", "NoCanLook" };
+		private static readonly string[] Salts = { "QJhwzAtXL", "XzUhGYdFp", "NoCanLook" }; //lol, new salt isn't really being used
 
 		public static List<byte[]> Retrieve(SQLiteConnection connection)
 		{
 			var userIds = new List<string>();
-
 			using (var cmd = new SQLiteCommand("select UserID from user", connection))
 			using (var reader = cmd.ExecuteReader())
 				while (reader.Read())
 					userIds.Add(reader["UserID"] as string);
-
 			using (var sha256 = new SHA256Managed())
 			{
-				List<string> deviceIds;
-				var pwsdid = GetPwsdidFromCookies();
-				if (!string.IsNullOrEmpty(pwsdid))
-					deviceIds = new List<string> {pwsdid};
-				else
-				{
-					var macs = NetworkInterface.GetAllNetworkInterfaces()
-						.Where(iface => iface.NetworkInterfaceType != NetworkInterfaceType.Loopback)
-						.Select(iface => iface.GetPhysicalAddress().ToMacString())
-						.Distinct()
-						.Where(addr => !string.IsNullOrEmpty(addr))
-						.ToList();
-					deviceIds = (
-						from mac in macs
-						from salt in Salts
-						let secret = salt + mac
-						let trimmedSecret = Encoding.UTF8.GetBytes(secret.Trim())
-						let hash = sha256.ComputeHash(trimmedSecret)
-						select hash.ToHexString()
-					).ToList();
-				}
-				var result = (from deviceId in deviceIds
+				var combinedSalt = Salts[1] + Salts[2];
+				var combinedHash = sha256.ComputeHash(Encoding.ASCII.GetBytes(combinedSalt)).ToHexString();
+				var realSalt = combinedHash.Substring(11, 9);
+				var mac = NetworkInterface.GetAllNetworkInterfaces()
+					.Where(iface => iface.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+					.Select(iface => iface.GetPhysicalAddress().ToMacString())
+					.Distinct()
+					.First(addr => !string.IsNullOrEmpty(addr));
+				var secret = realSalt + mac;
+				var deviceId = sha256.ComputeHash(Encoding.ASCII.GetBytes(secret)).ToHexString();
+
+				var result = (
 					from userId in userIds
 					select Encoding.UTF8.GetBytes((deviceId + userId).Trim()) into key
 					select sha256.ComputeHash(key) into hash
 					select hash.Copy(hash.Length - 16)
 				).ToList();
 
-				//var result = masterHashes.Select(hash => hash.Copy(hash.Length - 16)).ToList();
 				//var strResult = result.Select(b => b.ToHexString()).Distinct().ToList();
 				return result;
 			}
-		}
-
-		private static string GetPwsdidFromCookies()
-		{
-			var cookies = (string[])Microsoft.Win32.Registry.GetValue(@"HKEY_CURRENT_USER\Software\Kobo\Kobo Desktop Edition\Browser", "cookies", null);
-			if (cookies == null || cookies.Length == 0)
-				return null;
-
-			var pwsdid = cookies.FirstOrDefault(cookie => cookie.Contains("pwsdid"));
-			if (string.IsNullOrEmpty(pwsdid))
-				return null;
-				
-			var start = pwsdid.IndexOf('=');
-			var end = pwsdid.IndexOf(';', start);
-			var result = pwsdid.Substring(start + 1, end - start - 1);
-			return result;
 		}
 	}
 }
